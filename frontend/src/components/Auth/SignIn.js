d8f0aaaf558dddffda39e1d4f20e5867f1b09142
Rahal Bhupathi <rbhupat@clemson.edu>
2023-02-23 05:31:40 +0000

    Merged PR 4: Added input fields for sign up/sign in
    Added input fields for sign up/sign in
    
    Added input field for password in SignIn.js, Added input fields for SignUp.js. Still unformatted.

--- frontend/src/components/Auth/SignIn.js
+++ frontend/src/components/Auth/SignIn.js
@@ -3,9 +3,13 @@ import React from 'react';
 export default function SignIn() {
 
     return (
-        <div class="form-group">
+        <><div class="form-group">
             <label for="InputUsername">Username</label>
             <input type="username" class="form-control" id="InputUsername" placeholder="Enter Username" />
         </div>
+        <div class="form-group">
+            <label for="InputPassword">Password</label>
+            <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password" />
+        </div></>
     );
 }
