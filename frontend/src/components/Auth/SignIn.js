4b37b8ce2816c9ba1db945879690c0a4ac7e739f
Charles Germany <cgerman@clemson.edu>
2023-02-23 00:35:36 +0000

    Merged PR 3: I added a username field, but I did not format it
    I added a username field, but I did not format it
    at all. I think it would be easier to format everything at once.

--- frontend/src/components/Auth/SignIn.js
+++ frontend/src/components/Auth/SignIn.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export default function SignIn() {
+
     return (
-        <h1>Reached Sign In Component</h1>
+        <div class="form-group">
+            <label for="InputUsername">Username</label>
+            <input type="username" class="form-control" id="InputUsername" placeholder="Enter Username" />
+        </div>
     );
 }
