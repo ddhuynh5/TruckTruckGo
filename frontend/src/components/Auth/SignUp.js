250f2d9eca50ef1753c713be8167d864b0c68b97
Mark Sandomeno <msandom@clemson.edu>
2023-03-02 22:49:48 +0000

    Merged PR 10: Sprint 4 password/email/remember me validation

--- frontend/src/components/Auth/SignUp.js
+++ frontend/src/components/Auth/SignUp.js
@@ -1,99 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SignUp() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-   
-    // States for checking the errors
-    const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
-   
-    // Handling the name change
-    const handleName = (e) => {
-      setName(e.target.value);
-      setSubmitted(false);
-    };
-   
-    // Handling the email change
-    const handleEmail = (e) => {
-      setEmail(e.target.value);
-      setSubmitted(false);
-    };
-   
-    // Handling the password change
-    const handlePassword = (e) => {
-      setPassword(e.target.value);
-      setSubmitted(false);
-    };
-   
-    // Handling the form submission
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (name === '' || email === '' || password === '') {
-        setError(true);
-      } else {
-        setSubmitted(true);
-        setError(false);
-      }
-    };
-   
-    // Showing success message
-    const successMessage = () => {
-      return (
-        <div
-          className="success"
-          style={{
-            display: submitted ? '' : 'none',
-          }}>
-          <h1>User {name} successfully registered!!</h1>
-        </div>
-      );
-    };
-   
-    // Showing error message if error is true
-    const errorMessage = () => {
-      return (
-        <div
-          className="error"
-          style={{
-            display: error ? '' : 'none',
-          }}>
-          <h1>Please enter all the fields</h1>
-        </div>
-      );
-    };
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [error, setError] = useState(false);
 
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+    setNameError(false);
+  };
+
+  function Checkbox() {
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const handleCheckboxChange = (event) => {
+      setIsChecked(event.target.checked);
+    };
+  }
+
+  const handleEmail = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+    setSubmitted(false);
+
+    if (!emailValue.includes('@')) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
+  const handlePassword = (e) => {
+    const passwordValue = e.target.value;
+    setPassword(passwordValue);
+    setSubmitted(false);
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(passwordValue)) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setError(true);
+    } else if (nameError || emailError || passwordError) {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+  
+      // Check if "Remember Me" checkbox is checked
+      const rememberMe = document.getElementById('remember_me').checked;
+      console.log('Remember Me:', rememberMe);
+    }
+  };
+
+  const successMessage = () => {
     return (
-        <><h1>Reached Sign Up Page</h1><div className="form">
-            <div>
-                <h1>User Registration</h1>
-            </div>
-
-            {/* Calling to the methods */}
-            <div className="messages">
-                {errorMessage()}
-                {successMessage()}
-            </div>
-
-            <form>
-                {/* Labels and inputs for form data */}
-                <label className="label">Name</label>
-                <input onChange={handleName} className="input"
-                    value={name} type="text" />
-
-                <label className="label">Email</label>
-                <input onChange={handleEmail} className="input"
-                    value={email} type="email" />
-
-                <label className="label">Password</label>
-                <input onChange={handlePassword} className="input"
-                    value={password} type="password" />
-
-                <button onClick={handleSubmit} className="btn" type="submit">
-                    Submit
-                </button>
-            </form>
-        </div></>
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}
+      >
+        <h1>User {name} successfully registered!!</h1>
+      </div>
     );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}
+      >
+        <h1>Please enter all the fields correctly</h1>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <h1>Reached Sign Up Page</h1>
+      <div className="form">
+        <div>
+          <h1>User Registration</h1>
+        </div>
+        
+        <div className="messages">
+          {errorMessage()}
+          {successMessage()}
+        </div>
+
+        <form>
+          <label className="label">Name</label>
+          <input
+            onChange={handleName}
+            className="input"
+            value={name}
+            type="text"
+          />
+
+          <label className="label">Email</label>
+          <input
+            onChange={handleEmail}
+            className="input"
+            value={email}
+            type="email"
+          />
+          {emailError && (
+            <div className="error">Please enter a valid email address</div>
+          )}
+
+          <label className="label">Password</label>
+          <input
+            onChange={handlePassword}
+            className="input"
+            value={password}
+            type="password"
+          />
+          {passwordError && (
+            <div className="error">
+              Password must be at least 8 characters long and contain at least
+              one capital letter and one number
+            </div>
+          )}
+
+           <input type="checkbox" id ="remember_me" />Remember Me
+
+          <button onClick={handleSubmit} className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
