ae853d9a7387b445a70fe6e015a31e72068ed656
Dennis Huynh <ddhuynh@clemson.edu>
2023-03-04 07:53:05 +0000

    Merged PR 11: Fixing styling and errors from previous merge
    Fixing styling and errors from previous merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import SignUp from './components/Auth/SignUp';
 import SignIn from './components/Auth/SignIn';
-import Drivers from "./components/test/backend_test";
+import Drivers from './components/test / backend_test';
 import LandingPage from './components/Auth/LandingPage';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/drivers" element={<Drivers />} />
+        <Route exact path='/' element={<LandingPage />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/drivers' element={<Drivers />} />
       </Routes>
     </BrowserRouter>
   );
