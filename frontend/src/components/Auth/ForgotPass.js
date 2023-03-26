153fbf2a284f7883d13ea8b6323025dcc7975e8c
Dennis <ddhuynh@clemson.edu>
2023-03-26 04:58:16 -0400

    class -> className

--- frontend/src/components/Auth/ForgotPass.js
+++ frontend/src/components/Auth/ForgotPass.js
@@ -3,7 +3,7 @@ export default function ForgotPass() {
     const [email, setEmail] = useState('');
 
     const handleReset = () => {
-        var email = document.getElementById("InputEmail").value;
+        const email = document.getElementById("InputEmail").value;
         if (email === "") {
             document.getElementById("msg").innerHTML = "<center class='text'>Please enter an email</center>";
         } else {
@@ -13,31 +13,31 @@ export default function ForgotPass() {
     }
 
 
-return (
-    <div className='wrapper'>
-        <div class="banner"><h1>Route Rewards</h1></div>
-        <div className="container">
-        <form>
-        <h2>Forgot Password</h2>
-            <div className="form-group">
-            <label htmlFor="InputEmail">Email account is registered with:</label>
-            <input
-                type="email"
-                className="form-control"
-                id="InputEmail"
-                placeholder="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            </div>
-            <button type="reset" className="btn-reset" onClick={handleReset}>Reset Password</button>
-            <p><a href="SignUp">Don't have an account?</a></p>
-        </form>
-            <div className="msg">
+    return (
+        <div className='wrapper'>
+            <div className="banner"><h1>Route Rewards</h1></div>
+            <div className="container">
+                <form>
+                    <h2>Forgot Password</h2>
+                    <div className="form-group">
+                        <label htmlFor="InputEmail">Email account is registered with:</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="InputEmail"
+                            placeholder="Enter Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <button type="reset" className="btn-reset" onClick={handleReset}>Reset Password</button>
+                    <p><a href="SignUp">Don't have an account?</a></p>
+                </form>
+                <div className="msg">
                     <span id="msg"></span>
                     <br />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 }
