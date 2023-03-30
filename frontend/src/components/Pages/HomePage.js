69d2c6e1d5fdc71fe45952f37be75a7d394c43fb
RahalBhupathi <rbhupat@clemson.edu>
2023-03-30 15:22:59 -0400

    Restructured frontend to use reusable elements.

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -2,63 +2,109 @@ import styles from '../../App.css';
 
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import { Col, Container, Dropdown, ListGroup, Row } from "react-bootstrap";
 
 export default function HomePage() {
     const [search, setSearch] = useState('');
 
-const changePageSettings = () => {
-    window.location='/accountsettings';
-};
-const changePageHome = () => {
-    window.location='/cart';
-};
-const changePageSponsors = () => {
-    window.location='/sponsors';
-};
+    const changePageSettings = () => {window.location='/accountsettings';};
+    const changePageHome = () => {window.location='/cart';};
+    const changePageSponsors = () => {window.location='/sponsors';};
+    const changePagePoints = () => {window.location='/points';};
+    const handleSearch = (e) => {
+        const SearchValue = e.target.value;
+        setSearch(SearchValue);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (search === '') {
+            window.location='/home';
+        } else {
+            window.location= '/home#search='+ search;
+        }
+    };
 
-const changePagePoints = () => {
-    window.location='/points';
-};
-
-const handleSearch = (e) => {
-    const SearchValue = e.target.value;
-    setSearch(SearchValue);
-};
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-    if (search === '') {
-        window.location='/home';
-    } else {
-        window.location= '/home#search='+ search;
+    function Copyright(props) {
+        return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+            Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+        );
     }
-};
+
+    function Navbar(){
+        return(
+            <nav class="navbar navbar-dark navbar-expand p-0 bg-dark">
+                <div class="container-fluid">
+                    <ul class="navbar-nav d-none d-md-flex mr-auto">
+                        <li class="nav-item"><a class="nav-link" href="#" onClick={changePageSponsors} data-abc="true">Sponsors</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" onClick={changePagePoints} data-abc="true">Points</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" onClick={changePageHome} data-abc="true">My Cart</a></li>
+                    </ul>
+                    <ul class="navbar-nav d-flex align-items-center">
+                        <li class="nav-item">
+                            <div class="d-flex flex-row">
+                                <img src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg" class="rounded-circle" width="30"></img>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" onClick={changePageSettings} class="nav-link d-flex align-items-center" data-abc="true"><span>Truck Driver</span><i class='bx bxs-chevron-down'></i></a>
+                        </li>
+                    </ul>
+                </div> 
+            </nav> 
+        );
+    }
+
+    function Filter(){
+        return(
+            <Dropdown>
+                <button class="btnfilter">Filter</button>
+            </Dropdown>
+        );
+    }
+
+    function Categories(){
+        return(
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="#search/category/electronics">Electronics</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#search/category/clothes">Clothes</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#search/category/furniture">Furniture</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#search/category/phones">Phones</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#shoes">Shoes</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+
+    function ProductDisplay(){
+        return(
+            <ul class="images">
+                <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
+                <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
+                <li> Sample Product <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71IQ-aaDVhL.jpg"></img></li>
+                <li> Sample Product <img src="https://m.media-amazon.com/images/I/71cFohr-HmL.jpg"></img></li>
+                <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
+                <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
+            </ul>
+        );
+    }
 
 return(
     <div className='wrapper'>
         <h1 class='banner'>Route Rewards</h1>
         <div className="container">
             <header class="section-header">
-                <nav class="navbar navbar-dark navbar-expand p-0 bg-dark">
-                    <div class="container-fluid">
-                        <ul class="navbar-nav d-none d-md-flex mr-auto">
-                            <li class="nav-item"><a class="nav-link" href="#" onClick={changePageSponsors} data-abc="true">Sponsors</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#" onClick={changePagePoints} data-abc="true">Points</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#" onClick={changePageHome} data-abc="true">My Cart</a></li>
-                        </ul>
-                        <ul class="navbar-nav d-flex align-items-center">
-                            <li class="nav-item">
-                                <div class="d-flex flex-row">
-                                    <img src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg" class="rounded-circle" width="30"></img>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" onClick={changePageSettings} class="nav-link d-flex align-items-center" data-abc="true"><span>Truck Driver</span><i class='bx bxs-chevron-down'></i></a>
-                            </li>
-                        </ul>
-                    </div> 
-                </nav> 
-
+                <Navbar></Navbar>
                 <section class="header-main border-bottom bg-white">
                     <div class="container-fluid">
                         <div class="row p-2 pt-3 pb-3 d-flex align-items-center">
@@ -66,6 +112,7 @@ return(
                                 <div class="d-flex form-inputs">
                                     <input class="searchBar" type="text" onChange={handleSearch} placeholder="Search any product..."></input>
                                     <button class = "searchButton" onClick={handleSubmit}>Search</button>
+                                    <Filter></Filter>
                                 </div>
                             </div>
                             <div class="col-md-2">
@@ -77,46 +124,15 @@ return(
                         </div>
                     </div> 
                 </section>
-
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <ul class="navbar-nav">
-                            <li class="nav-item"><a class="nav-link" href="#search/category/electronics">Electronics</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#search/category/clothes">Clothes</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#search/category/furniture">Furniture</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#search/category/phones">Phones</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#shoes">Shoes</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                <Categories></Categories>
             </header>
 
             <container>
-                <p class = "homepagetxt">New Products</p>
-                <ul class="images">
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71IQ-aaDVhL.jpg"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/I/71cFohr-HmL.jpg"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                </ul>
-                <ul class="images">
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71IQ-aaDVhL.jpg"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/I/71cFohr-HmL.jpg"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                </ul>
-                <ul class="images">
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71IQ-aaDVhL.jpg"></img></li>
-                    <li> Sample Product <img src="https://m.media-amazon.com/images/I/71cFohr-HmL.jpg"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.52.37PM_530x@2x.png?v=1657904015"></img></li>
-                    <li> Sample Product <img src="https://cdn.shopify.com/s/files/1/0010/5457/2601/products/ScreenShot2022-07-15at12.45.12PM_530x@2x.png?v=1657903564"></img></li>
-                </ul>
+                <p class = "homepagetxt">New Products</p>        
+                <ProductDisplay></ProductDisplay>
+                <ProductDisplay></ProductDisplay>
+                <ProductDisplay></ProductDisplay>
+                <Copyright sx={{ pt: 4 }} />
             </container>
         </div>
     </div>
