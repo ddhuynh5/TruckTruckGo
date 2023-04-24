b732feab6e1065e0ff19e18e3621100bcd3b3528
Dennis Huynh <ddhuynh@clemson.edu>
2023-04-24 02:59:19 +0000

    Merged PR 57: added FAQ

--- frontend/src/components/Pages/Points.js
+++ frontend/src/components/Pages/Points.js
@@ -1,54 +1,35 @@
 import '../../App.css';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './Header';
-import { points } from './PagesHelper';
-import Cookies from "js-cookie";
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function PointsPage() {
-    const [totalPoints, setTotalPoints] = useState("");
-    const [fullName, setFullName] = useState("");
-    const [id, setId] = useState("");
-
-    useEffect(() => {
-        const name = Cookies.get('name');
-        const id = Cookies.get('uniqueId');
-        setId(id);
-        setFullName(name);
-    }, []);
-    useEffect(() => {
-        const getPoints = async () => {
-            const pointData = await points(id);
-            setTotalPoints(pointData[0].total_points);
-        }
-
-        if (id) {
-            getPoints();
-        }
-    }, [id]);
-
-
-    function PointDisplay() {
-        return (
-            <div className="card p-3">
-                <p className="text-dark">{fullName}</p>
-                <div className="card-bottom pt-3 px-3 mb-2">
-                    <div className="d-flex flex-row justify-content-between text-align-center">
-                        <div className="d-flex flex-column"><span>My Points:</span><p><span className="text-white">{totalPoints}</span></p></div>
-                    </div>
-                </div>
-                <span>Earn more points from good driving!</span>
-            </div>
-        );
-    }
 
     return (
-        <div className='wrapper'>
+        <Container fluid className="p-0">
             <Header />
-            <div className="container">
-                <header className="section-header"></header>
-                <span> <PointDisplay /></span>
+            <div className="py-5">
+                <Container>
+                    <Row className="justify-content-center align-items-center">
+                        <Col lg={7}>
+                            <h1 className="display-3 mb-5">FAQ</h1>
+                            <p className="lead"><strong>Q: How can I earn points in the app?</strong></p>
+                            <p className="lead mb-5">A: You can earn points by completing deliveries, driving safely, and referring other drivers to the app. Each completed delivery and safe driving behavior will earn you a certain number of points, and you'll receive a bonus for each new driver you refer.</p>
+                            <p className="lead"><strong>Q: How do I know how many points I have?</strong></p>
+                            <p className="lead mb-5">A: Your current point balance will be displayed on the app's home screen. You can also view your transaction history to see how you earned and used points over time.</p>
+                            <p className="lead"><strong>Q: What can I use my points for?</strong></p>
+                            <p className="lead mb-5">A: You can redeem your points for various rewards, such as gift cards, fuel discounts, and access to exclusive promotions. The available rewards and redemption options may vary over time, so be sure to check the app frequently for updates.</p>
+                            <p className="lead"><strong>Q: Can I transfer my points to another driver?</strong></p>
+                            <p className="lead mb-5">A: No, points are non-transferable and can only be used by the driver who earned them. However, you can refer other drivers to the app and earn bonus points when they sign up and start using the app.</p>
+                            <p className="lead"><strong>Q: Can I use my points to pay for tolls or parking fees?</strong></p>
+                            <p className="lead mb-5">A: Not currently, but we're working on adding more redemption options in the future. For now, you can use your points to save on fuel costs or other rewards offered in the app.</p>
+                            <p className="lead"><strong>Q: What happens if I don't use my points for a long time?</strong></p>
+                            <p className="lead mb-5">A: Your points will not expire as long as you continue to use the app and earn points through deliveries and safe driving. However, we reserve the right to modify the point expiration policy at any time, so be sure to check the app's terms and conditions for the latest information.</p>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-        </div>
+        </Container>
     )
 }
--- frontend/src/components/Pages/Sponsors.js
+++ frontend/src/components/Pages/Sponsors.js
@@ -1,9 +1,24 @@
 import '../../App.css';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import { all_sponsors } from '../settings/SettingsHelper';
+import { Card } from 'react-bootstrap';
+import { FaAmazon } from 'react-icons/fa';
+import { SiNike } from 'react-icons/si';
+import { AiFillApple, AiFillHome } from 'react-icons/ai'
 
 export default function Sponsors() {
+    const [sponsors, setSponsors] = useState([]);
+
+    useEffect(() => {
+        const fetchSponsors = async () => {
+            const response = await all_sponsors();
+            setSponsors(response.all_sponsors);
+        };
+        fetchSponsors();
+    }, []);
+
     return (
         <div className='wrapper'>
             <Header />
@@ -11,6 +26,62 @@ export default function Sponsors() {
                 <header className="section-header">
                     <h1>Sponsors</h1>
                 </header>
+                <ul className="image-gallery">
+                    {sponsors.map((sponsor, index) => (
+                        <li key={sponsor.sponsor_id}>
+                            <Card>
+                                <Card.Body style={{ display: 'flex', alignItems: 'center' }}>
+                                    {sponsor.sponsor_name === "Amazon" && (
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <FaAmazon style={{ marginRight: '50px' }} />
+                                            <div>
+                                                <Card.Title>{sponsor.sponsor_name}</Card.Title>
+                                                <div>Retail - Online - Services</div>
+                                            </div>
+                                        </div>
+                                    )}
+                                    {sponsor.sponsor_name === "Nike" && (
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <SiNike style={{ marginRight: '50px' }} />
+                                            <div>
+                                                <Card.Title>{sponsor.sponsor_name}</Card.Title>
+                                                <div>Athletic - Innovative - Partnership</div>
+                                            </div>
+                                        </div>
+                                    )}
+                                    {sponsor.sponsor_name === "Apple" && (
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <AiFillApple style={{ marginRight: '50px' }} />
+                                            <div>
+                                                <Card.Title>{sponsor.sponsor_name}</Card.Title>
+                                                <div>Technology - Design - Services</div>
+                                            </div>
+                                        </div>
+                                    )}
+                                    {sponsor.sponsor_name === "Lowe's" && (
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <AiFillHome style={{ marginRight: '50px' }} />
+                                            <div>
+                                                <Card.Title>{sponsor.sponsor_name}</Card.Title>
+                                                <div>Home Improvement - Retail - DIY (Do It Yourself)</div>
+                                            </div>
+                                        </div>
+                                    )}
+                                    {sponsor.sponsor_name !== "Amazon" && sponsor.sponsor_name !== "Nike" && sponsor.sponsor_name !== "Apple" && sponsor.sponsor_name !== "Lowe's" && (
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <div style={{ marginRight: '50px' }}><strong>{sponsor.sponsor_name.charAt(0)}</strong></div>
+                                            <div>
+                                                <Card.Title>{sponsor.sponsor_name}</Card.Title>
+                                                <div style={{ height: '20px' }}></div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </Card.Body>
+
+                            </Card>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
