d830b2c68cb9fec0a58a9ec2d9c2f5cc944f4b79
Dennis <ddhuynh@clemson.edu>
2024-03-21 15:07:26 -0400

    updated footer to not show during overlay

--- frontend/src/components/PageComponents/Footer.js
+++ frontend/src/components/PageComponents/Footer.js
@@ -7,7 +7,7 @@ export default function Footer(props) {
     const currentYear = new Date().getFullYear();
 
     const handleSubscribe = () => {
-        toast.success("Thank you for subscribing!", {
+        toast.info("Subscription service coming soon!", {
             closeButton: false
         });
     };
@@ -99,10 +99,10 @@ export default function Footer(props) {
                         <div className="flex flex-wrap mb-4">
                             <div className="w-full">
                                 <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
-                                <div className="relative flex items-center max-w-xs">
+                                <div className="flex items-center max-w-xs">
                                     <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                                    <button onClick={handleSubscribe} className="absolute inset-0 left-auto" aria-label="Subscribe">
-                                        <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
+                                    <button onClick={handleSubscribe} aria-label="Subscribe">
+                                        <span className="w-2 h-6 bg-gray-300" aria-hidden="true" />
                                         <svg className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                                         </svg>
