7cb8aea468b22076be643128e7a5face924de32e
Dennis <ddhuynh@clemson.edu>
2024-03-21 03:22:37 -0400

    finished update account settings, in prog - deactivation

--- frontend/src/components/Pages/Settings.js
+++ frontend/src/components/Pages/Settings.js
@@ -1,40 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../PageComponents/Header";
 import Footer from "../PageComponents/Footer";
 import Divider from "../PageComponents/Divider";
 import Loading from "../PageComponents/Loading";
 import { fetchLoginInfo } from "../Helpers/AuthHelper";
-import { sponsor, update } from "../Helpers/SettingsHelper";
+import { update, sponsor, all_sponsors, driver } from "../Helpers/SettingsHelper";
 import Select from "react-select";
 
 const Settings = () => {
     const navigate = useNavigate();
+    const modalRef = useRef(null);
+
     const [loading, setLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
     const [id, setId] = useState("");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [first, setFirst] = useState("");
     const [last, setLast] = useState("");
-    const [role, setRole] = useState("");
+    const [address, setAddress] = useState("");
     const [currentSponsor, setCurrentSponsor] = useState("");
+
+    const [defaults, setDefaults] = useState({});
     const [sponsors, setSponsors] = useState([]);
 
-    const handleUpdate = () => {
+    const openModal = () => {
+        setIsOpen(true);
+    };
 
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
+    const handleUpdate = () => {
+        if (
+            first !== defaults.first ||
+            last !== defaults.last ||
+            email !== defaults.email ||
+            address !== defaults.address ||
+            currentSponsor.value !== defaults.sponsor ||
+            password !== ""
+        ) {
+            const updatedUserInfo = {
+                first_name: first,
+                last_name: last,
+                email: email,
+                password: password,
+                address: address,
+                role_id: 3,
+                sponsor_id: currentSponsor.value
+            };
+
+            update(id, updatedUserInfo);
+        }
+    };
+
+    const handleDeactivate = () => {
+        console.log("HI")
     };
 
     const handleSelectChange = (selectedOption) => {
-        setCurrentSponsor(selectedOption.value);
+        setCurrentSponsor(selectedOption);
     };
 
-    const fetchUserInfo = () => {
-        setEmail(localStorage.getItem("email") || sessionStorage.getItem("email"));
-        setRole(localStorage.getItem("role") || sessionStorage.getItem("role"));
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                closeModal();
+            }
+        }
 
-        const name = localStorage.getItem("name") || sessionStorage.getItem("name");
-        setFirst(name.split(" ")[0]);
-        setLast(name.split(" ")[1]);
-    };
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     useEffect(() => {
         const info = fetchLoginInfo();
@@ -49,24 +92,44 @@ const Settings = () => {
     useEffect(() => {
         async function fetchData() {
             setLoading(true);
-            if (id) {
-                const response = await sponsor(id);
-                setCurrentSponsor(response["current_sponsor"][0]["sponsor_name"]);
 
-                let sponsorsList = [];
-                for (let sponsor of response["all_sponsors"]) {
-                    sponsorsList.push(sponsor["sponsor_name"]);
-                }
+            const sponsorInfo = await sponsor(id);
+            setCurrentSponsor({
+                label: sponsorInfo[0]["sponsor_name"],
+                value: sponsorInfo[0]["sponsor_id"]
+            });
 
-                const mappedSponsors = sponsorsList.map(item => ({ value: item, label: item }));
-
-                setSponsors(mappedSponsors);
+            const all = await all_sponsors(id);
+            let sponsorsList = [];
+            for (let sponsor of all["all_sponsors"]) {
+                sponsorsList.push({
+                    label: sponsor["sponsor_name"],
+                    value: sponsor["sponsor_id"]
+                });
             }
-            setLoading(false);
-        };
 
-        fetchData();
-        fetchUserInfo();
+            setSponsors(sponsorsList);
+
+            const driverInfo = await driver(id);
+            setFirst(driverInfo[0].first_name);
+            setLast(driverInfo[0].last_name)
+            setEmail(driverInfo[0].email)
+            setAddress(driverInfo[0].address);
+
+            setDefaults({
+                first: driverInfo[0].first_name,
+                last: driverInfo[0].last_name,
+                email: driverInfo[0].email,
+                address: driverInfo[0].address,
+                sponsor: sponsorInfo[0]["sponsor_id"]
+            });
+
+            setLoading(false);
+        }
+
+        if (id) {
+            fetchData();
+        }
     }, [id]);
 
     return (
@@ -82,17 +145,18 @@ const Settings = () => {
 
                             <Divider />
 
-                            {/* !loading */true ? (
+                            {/* !loading */ true ? (
                                 <div className="mt-4 space-y-8">
-                                    <div className="grid grid-cols-1 md:grid-cols-4 justify-items-center space-y-4 md:space-y-0">
+                                    <div className="grid grid-cols-1 md:grid-cols-3 space-y-4 md:space-y-2">
                                         <div className="col-span-1">
                                             <label htmlFor="first" className="block text-sm font-medium text-gray-700">First Name</label>
                                             <input
                                                 type="text"
                                                 id="first"
                                                 className="shadow-sm rounded-md px-3 py-2 border border-gray-300 
-                                                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                                                placeholder={first}
+                                                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                                                placeholder={defaults.first}
+                                                onChange={(event) => { setFirst(event.target.value) }}
                                                 required
                                             />
                                         </div>
@@ -102,8 +166,9 @@ const Settings = () => {
                                                 type="text"
                                                 id="last"
                                                 className="shadow-sm rounded-md px-3 py-2 border border-gray-300 
-                                                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 md:mb-4"
-                                                placeholder={last}
+                                                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 md:mb-4"
+                                                placeholder={defaults.last}
+                                                onChange={(event) => { setLast(event.target.value) }}
                                                 required
                                             />
                                         </div>
@@ -113,52 +178,91 @@ const Settings = () => {
                                                 type="email"
                                                 id="email"
                                                 className="shadow-sm rounded-md px-3 py-2 border border-gray-300 
-                                                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                                                placeholder={email}
+                                                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                                                placeholder={defaults.email}
+                                                onChange={(event) => { setEmail(event.target.value) }}
                                                 required
                                             />
                                         </div>
                                         <div className="col-span-1">
-                                            <label htmlFor="role" className="block text-sm font-medium text-gray-700">Role</label>
+                                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">New Password</label>
                                             <input
                                                 type="text"
-                                                id="role"
+                                                id="password"
                                                 className="shadow-sm rounded-md px-3 py-2 border border-gray-300 
-                                                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                                                placeholder={role}
+                                                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                                                placeholder="Set a new password"
+                                                onChange={(event) => { setPassword(event.target.value) }}
                                                 required
                                             />
                                         </div>
                                         <div className="col-span-1">
-                                            <Select
-                                                id="new-quantity"
-                                                options={sponsors}
-                                                value={sponsors.find(sponsor => sponsor.value === currentSponsor)}
-                                                onChange={handleSelectChange}
-                                                placeholder={currentSponsor}
-                                                className=""
-                                            />
-                                        </div>
-                                        <div className="col-span-1">
-                                            <label htmlFor="role" className="block text-sm font-medium text-gray-700">Role</label>
+                                            <label htmlFor="address" className="block text-sm font-medium text-gray-700">Address</label>
                                             <input
                                                 type="text"
-                                                id="role"
+                                                id="address"
                                                 className="shadow-sm rounded-md px-3 py-2 border border-gray-300 
-                                                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                                                placeholder={role}
+                                                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                                                placeholder={defaults.address}
+                                                onChange={(event) => { setAddress(event.target.value) }}
                                                 required
                                             />
+                                        </div>
+                                        <div className="col-span-1 md:pt-1">
+                                            <label htmlFor="sponsors" className="block text-sm font-medium text-gray-700">Company</label>
+                                            <div className="w-56">
+                                                <Select
+                                                    id="sponsor"
+                                                    options={sponsors}
+                                                    value={currentSponsor}
+                                                    onChange={handleSelectChange}
+                                                    placeholder={currentSponsor}
+                                                />
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="flex md:justify-end justify-center">
+                                    <div className="flex md:justify-end">
                                         <button
-                                            /* onClick={submitReset} */
+                                            onClick={handleUpdate}
                                             className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 
-                                                font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                                        font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                                         >
                                             Update
                                         </button>
+                                        <button
+                                            onClick={openModal}
+                                            className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 
+                                                        font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-4"
+                                        >
+                                            Deactivate Account
+                                        </button>
+                                        {isOpen && (
+                                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+                                                <div className="fixed inset-0 flex items-center justify-center">
+                                                    <div ref={modalRef} className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:max-w-lg">
+                                                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                            <div className="sm:flex sm:items-start">
+                                                                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                                    <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                                                    </svg>
+                                                                </div>
+                                                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                                    <h3 className="text-base font-semibold leading-6 text-gray-900">Deactivate account</h3>
+                                                                    <div className="mt-2">
+                                                                        <p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                            <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" onClick={handleDeactivate}>Deactivate</button>
+                                                            <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={closeModal}>Cancel</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                             ) : (
