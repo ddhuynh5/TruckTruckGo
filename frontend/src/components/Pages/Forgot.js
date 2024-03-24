5ae53ac37c710fc7f10878d6171eebac3bcdbb44
Dennis <ddhuynh@clemson.edu>
2024-03-23 20:35:37 -0400

    added link to signin

--- frontend/src/components/Pages/Forgot.js
+++ frontend/src/components/Pages/Forgot.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Forgot = () => {
+    const navigate = useNavigate();
 
     const submitReset = async () => {
 
@@ -47,7 +49,16 @@ const Forgot = () => {
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
                             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="your@email.com" required={true} />
                         </div>
-                        <button onClick={submitReset} className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Reset password</button>
+                        <span
+                            onClick={() => { navigate("/signin") }}
+                            className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 
+                                                        focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer underline mb-4
+                                                        float-right"
+                        >
+                            Already have an account?
+                        </span>
+                        <button onClick={submitReset} className="w-full text-white bg-indigo-600 hover:bg-indigo-700 
+                                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Reset password</button>
                     </form>
                 </div>
             </div>
